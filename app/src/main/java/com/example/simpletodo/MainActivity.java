c9e29d9680e2b4545f7ad3b798eb2d190b20aaab
Saad Mahboob <smahboob@fandm.edu>
2021-01-29 17:06:42 -0500

    Local File Storage Added to save todo list

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -10,6 +10,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,13 +30,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        itemsList = new ArrayList<>();
         textEntered = findViewById(R.id.enterItemText);
         recyclerView = findViewById(R.id.ListViewRecycle);
 
-        itemsList.add("Buy Milk");
-        itemsList.add("Complete Computer Science Homework");
-        itemsList.add("Make dinner by 6:00");
+        loadItems();
 
         updateAdapters();
 
@@ -42,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
             itemsList.add(textEntered.getText().toString());
             textEntered.setText("");
             itemAdapter.notifyItemInserted(itemsList.size()-1);
+            saveData();
             Toast.makeText(MainActivity.this, "Item Successfully Added!", Toast.LENGTH_LONG).show();
         }
         else{
@@ -56,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
             if(position < itemsList.size()) {
                 itemsList.remove(position);
                 itemAdapter.notifyItemRemoved(position);
+                saveData();
                 Toast.makeText(MainActivity.this, "Item Successfully Removed!", Toast.LENGTH_LONG).show();
             }
         };
@@ -63,5 +67,31 @@ public class MainActivity extends AppCompatActivity {
         itemAdapter = new ItemAdapter(itemsList, onLongClickListener);
         recyclerView.setAdapter(itemAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    //this will return the file which will store the items from our lis
+    private File getDataFile(){
+        //pass the directory and the name of the file in the parameters
+        File file = new File(getApplicationContext().getFilesDir(), "todoData.txt");
+        return file;
+    }
+
+    //read all the lines in the data and load it
+    private void loadItems(){
+        try{
+            itemsList = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        }
+        catch (IOException e){
+            itemsList = new ArrayList<>();
+        }
+    }
+
+    //save all the items by writing them into the file
+    private void saveData(){
+        try {
+            FileUtils.writeLines(getDataFile(), itemsList);
+        } catch (IOException e) {
+            Toast.makeText(MainActivity.this, "Unable to save data!", Toast.LENGTH_LONG).show();
+        }
     }
 }
